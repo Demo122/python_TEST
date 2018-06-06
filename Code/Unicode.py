863a23a24e9f54eeca94fb98beca78112f195595
Demo122 <1094346704@qq.com>
2018-06-06 16:43:16 +0800

    熟悉更多字符的Unicode码点和名称， 参考网站http://unicode-table.com

--- Code/Unicode.py
+++ Code/Unicode.py
@@ -4,6 +4,8 @@
 @auther:danqing
 @time:2018/6/6 12:30
 """
-print("\U0001F60A")  #  打印了一个笑脸 😊
+print("笑脸：\U0001F60A")  #  打印了一个笑脸 😊
 print("this is a cat : \N{Cat}")  #this is a cat : 🐈
+print("\U0001F638")
 
+#熟悉更多字符的Unicode码点和名称，参考网站 http://unicode-table.com
