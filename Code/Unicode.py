1880cade284ee5155b7246f5410f468975386319
Demo122 <1094346704@qq.com>
2018-06-06 12:44:23 +0800

    初识unicode码点和名称

---
+++ Code/Unicode.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@auther:danqing
+@time:2018/6/6 12:30
+"""
+print("\U0001F60A")  #  打印了一个笑脸 😊
+print("this is a cat : \N{Cat}")  #this is a cat : 🐈
+
