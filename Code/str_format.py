f1ee5dee531bc6524a8c9dca946c35de7784ab6c
Demo122 <1094346704@qq.com>
2018-06-08 14:34:30 +0800

    字符串格式设置示例，format探究

--- Code/str_format.py
+++ Code/str_format.py
@@ -37,3 +37,26 @@ print(str6) #Euler's constant is roughly 2.718281828459045
 # 船舰最终字符串时，将把替换字段e替换为变量e的值，与下面等价
 str7 = "Euler's constant is roughly {e}.".format(e=e)
 print(str7) # Euler's constant is roughly 2.718281828459045.
+
+
+#字符串格式设置示例
+
+#根据指定的宽度打印格式良好的列表
+width = int(input("Please enter width: "))
+price_width = 10
+item_width = width - price_width
+
+header_fmt = '{{:{}}} {{:>{}}}'.format(item_width,price_width)
+fmt        = '{{:{}}} {{:>{}.2f}}'.format(item_width,price_width)
+
+print('='*width)
+print(header_fmt.format('item','price'))
+print('-'*width)
+
+print(fmt.format('Apples',0.4))
+print(fmt.format('Pears',0.5))
+print(fmt.format('Cantaloupes',1.92))
+print(fmt.format('Dried Apricots (16 oz.)',8))
+print(fmt.format('Computer',45))
+
+print('='*width)
