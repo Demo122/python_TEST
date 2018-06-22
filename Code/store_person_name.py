c0bce4fe25d3a2699af3a6d0337d4cdb183c2eba
Demo122 <1094346704@qq.com>
2018-06-23 00:28:39 +0800

    收集参数，*，**练习补充（**用于收集关键字参数）

--- Code/store_person_name.py
+++ Code/store_person_name.py
@@ -24,6 +24,21 @@ def store(data,full_name):
         else:
             data[label][name] = [full_name]
 
+def store2(data,*full_names):
+    #改进后，可以接受存储多个名字
+    for full_name in full_names:
+        names = full_name.split()
+        if len(names) == 2: names.insert(1, '')
+        labels = 'first', 'middle', 'last'
+
+        for label, name in zip(labels, names):
+            people = lookup(data, label, name)
+            if people:
+                people.append(full_name)
+            else:
+                data[label][name] = [full_name]
+
+
 if __name__ == '__main__':
     storage = {}
     init(storage)
@@ -39,5 +54,11 @@ if __name__ == '__main__':
     people2 = lookup(storage,'middle','')
     print(people2)
     print(storage)
+
+    d={}
+    init(d)
+    store2(d,'Luck Skywalker','Anakin Skywalker')
+    people3 = lookup(d,'last','Skywalker')
+    print(people3)
 
 
