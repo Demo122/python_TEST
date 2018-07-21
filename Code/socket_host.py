3c4fe990d93d60451b955c4871e45f60a47bf71f
Demo122 <1094346704@qq.com>
2018-07-21 16:07:49 +0800

    使用socketserver创建简单的服务器程序

--- Code/socket_host.py
+++ Code/socket_host.py
@@ -5,22 +5,33 @@
 @time:2018/7/17 16:36
 """
 
-import socket,threading
+# import socket,threading
+#
+# def do_send_recv(c,addr):
+#     print("connection from %s: %s",addr)
+#     c.send("欢迎连接第一个服务器".encode('utf-8'))
+#     data=c.recv(1024)
+#     c.send(("Hello, %s!"%data.decode('utf-8')).encode('utf-8'))
+#
+# s = socket.socket()
+# #host = socket.gethostname()
+# port = 9999
+# s.bind(('74.120.172.167', port))
+#
+# s.listen(5)
+#
+# while True:
+#     c, addr = s.accept()
+#     t=threading.Thread(target=do_send_recv,args=(c,addr))
+#     t.start()
 
-def do_send_recv(c,addr):
-    print("connection from %s: %s",addr)
-    c.send("欢迎连接第一个服务器".encode('utf-8'))
-    data=c.recv(1024)
-    c.send(("Hello, %s!"%data.decode('utf-8')).encode('utf-8'))
+from socketserver import TCPServer,StreamRequestHandler
 
-s = socket.socket()
-#host = socket.gethostname()
-port = 9999
-s.bind(('74.120.172.167', port))
+class Handler(StreamRequestHandler):
+    def handle(self):
+        addr=self.request.getpeername()
+        print("Got connection from",addr)
+        self.wfile.write(b'Thank you for connecting')
 
-s.listen(5)
-
-while True:
-    c, addr = s.accept()
-    t=threading.Thread(target=do_send_recv,args=(c,addr))
-    t.start()
+server = TCPServer(('DESKTOP-FRIPQFS',9999),Handler)
+server.serve_forever()
